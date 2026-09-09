5d59a347f551e2840648a71003ec3a87a8b4804b
Ankit Varshney <ankit.varshney@nagarro.com>
2026-09-09 18:37:41 +0530

    group

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -3,12 +3,19 @@ import org.testng.Assert;
 
 public class BasicTest {
 
-		@Test
+		@Test(groups = "smoke")
 		public void firstTest()
 		{
-			System.out.println("test by ankit");
+			System.out.println("smoke test");
 			String environment = System.getenv("Environment");
 			System.out.println("Running Test on " +  environment);
+		}
+
+		@Test(groups = "Regression")
+		public void secondTest()
+		{
+			System.out.println("regression test");
+
 		}
 
 		public static void main(String[] args)
