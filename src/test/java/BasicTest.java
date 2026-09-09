3165581d28c85d9c3034507e588a33d5f9532d98
Ankit Varshney <ankit.varshney@nagarro.com>
2026-09-09 16:21:00 +0530

    secret

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -9,6 +9,8 @@ public class BasicTest {
 			System.out.println("test by ankit");
 			String environment = System.getenv("Environment");
 			System.out.println("Running Test on " +  environment);
+			String Password = System.getenv("Test_Password");
+			System.out.println("Password received: " + Password);
 		}
 
 		public static void main(String[] args)
