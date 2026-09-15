583543d7d5469cded84685015cb326df78a717eb
Ankit Varshney <ankit.varshney@nagarro.com>
2026-09-15 17:38:25 +0530

    ss1

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class BrowserTest {
@@ -7,7 +8,14 @@ public class BrowserTest {
      @Test(groups = "smoke")
   public void openBrowser() throws Exception {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+WebDriver driver = new ChromeDriver(options);
+
+driver.get("https://www.google.com");
 
         driver.get("https://www.google.com");
 
